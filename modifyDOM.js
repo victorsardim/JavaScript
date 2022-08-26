560bb847d1129bcf5bce5bf5d400a4934e77fc50
Victor Sardim <victorsardim@gmail.com>
2022-08-25 23:40:41 -0300

    aula 13 - manipulando DOM

---
+++ modifyDOM.js
@@ -0,0 +1,19 @@
+const items = document.querySelector('.items');
+console.log(items);
+
+// items.remove();
+
+// remover o 1o li
+// items.firstElementChild.remove();
+// // remover o ultimo li
+// items.firstElementChild.remove();
+
+// console.log(items.children);
+
+// items.children[1].textContent = 'mudei o item 2 - pos 1';
+
+// items.lastElementChild.innerHTML = ' <h1>Hello World!</h1>';
+const button = document.querySelector('.btn');
+console.log(button);
+button.style.background = 'green';
+button.style.color = 'yellow';
